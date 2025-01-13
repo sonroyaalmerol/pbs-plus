a2d22e84e347317ec6487a62be6271c5912a582a
Son Roy Almerol <sralmerol@sgl.com>
2025-01-13 16:22:53 -0500

    recover from panic and print stack to event logs

--- cmd/windows_agent/main.go
+++ cmd/windows_agent/main.go
@@ -7,20 +7,70 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"runtime/debug"
 	"time"
 
 	"github.com/kardianos/service"
 	"github.com/sonroyaalmerol/pbs-plus/internal/syslog"
 	"golang.org/x/sys/windows/registry"
+	"golang.org/x/sys/windows/svc/eventlog"
 )
 
 var Version = "v0.0.0"
+var eventLogger *eventlog.Log
+
+const (
+	eventSourceKey = "PBSPlusAgent"
+)
+
+func initEventLog() error {
+	// Register event source
+	err := eventlog.InstallAsEventCreate(eventSourceKey, eventlog.Error|eventlog.Warning|eventlog.Info)
+	if err != nil {
+		return fmt.Errorf("failed to install event source: %v", err)
+	}
+
+	// Open event log
+	logger, err := eventlog.Open(eventSourceKey)
+	if err != nil {
+		return fmt.Errorf("failed to open event log: %v", err)
+	}
+	eventLogger = logger
+	return nil
+}
+
+func logPanic(err interface{}) {
+	if eventLogger != nil {
+		stack := debug.Stack()
+		errorMsg := fmt.Sprintf("PBS Plus Agent Panic: %v\nStack Trace:\n%s", err, stack)
+		_ = eventLogger.Error(1, errorMsg)
+	}
+}
+
+func setupPanicHandler() {
+	if r := recover(); r != nil {
+		logPanic(r)
+		os.Exit(1)
+	}
+}
 
 func main() {
+	// Initialize Windows Event Log
+	if err := initEventLog(); err != nil {
+		fmt.Printf("Failed to initialize event log: %v\n", err)
+	}
+	defer func() {
+		if eventLogger != nil {
+			eventLogger.Close()
+		}
+	}()
+
+	// Set up panic handler
+	defer setupPanicHandler()
+
 	execPath, _ := os.Executable()
 	fileName := path.Base(execPath)
 	filePath := path.Dir(execPath)
-
 	fullOldPath := path.Join(filePath, "."+fileName+".old")
 	_ = os.RemoveAll(fullOldPath)
 
@@ -34,6 +84,9 @@ func main() {
 	prg := &agentService{}
 	s, err := service.New(prg, svcConfig)
 	if err != nil {
+		if eventLogger != nil {
+			_ = eventLogger.Error(1, fmt.Sprintf("Failed to initialize service: %v", err))
+		}
 		fmt.Println("Failed to initialize service:", err)
 		return
 	}
@@ -41,22 +94,28 @@ func main() {
 
 	logger, err := syslog.InitializeLogger(s)
 	if err != nil {
+		if eventLogger != nil {
+			_ = eventLogger.Error(1, fmt.Sprintf("Failed to initialize logger: %v", err))
+		}
 		fmt.Println("Failed to initialize logger:", err)
 		return
 	}
 
-	//tray := &agentTray{svc: s, ctx: context.Background()}
-
 	if len(os.Args) > 1 && os.Args[1] == "--set-server-url" {
 		if !isAdmin() {
 			logger.Error("Needs to be running as administrator.")
+			if eventLogger != nil {
+				_ = eventLogger.Error(1, "Failed to set server URL: Not running as administrator")
+			}
 			return
 		}
-
 		if len(os.Args) > 2 {
 			serverUrl := os.Args[2]
 			if err := setServerURLAdmin(serverUrl); err != nil {
 				logger.Errorf("Error setting server URL: %s", err)
+				if eventLogger != nil {
+					_ = eventLogger.Error(1, fmt.Sprintf("Error setting server URL: %v", err))
+				}
 			}
 		}
 		return
@@ -69,20 +128,20 @@ func main() {
 				_ = registry.DeleteKey(registry.LOCAL_MACHINE, fmt.Sprintf(`Software\PBSPlus\Config\SFTP-%s`, drive))
 			}
 		}
-
 		err = service.Control(s, cmd)
 		if err != nil {
 			logger.Errorf("Failed to %s service: %s", cmd, err)
+			if eventLogger != nil {
+				_ = eventLogger.Error(1, fmt.Sprintf("Failed to %s service: %v", cmd, err))
+			}
 			return
 		}
-
 		if cmd == "install" {
 			go func() {
 				<-time.After(10 * time.Second)
 				_ = s.Start()
 			}()
 		}
-
 		return
 	}
 
@@ -90,15 +149,12 @@ func main() {
 		err = s.Run()
 		if err != nil {
 			logger.Errorf("Error running service: %s", err)
+			if eventLogger != nil {
+				_ = eventLogger.Error(1, fmt.Sprintf("Error running service: %v", err))
+			}
 		}
 	} else {
 		return
-		/*
-			err = tray.foregroundTray()
-			if err != nil {
-				logger.Errorf("Error running tray: %s", err)
-			}
-		*/
 	}
 }
 
@@ -116,10 +172,8 @@ func setServerURLAdmin(serverUrl string) error {
 		return fmt.Errorf("setServerURLAdmin: error creating HKLM key -> %w", err)
 	}
 	defer key.Close()
-
 	if err := key.SetStringValue("ServerURL", serverUrl); err != nil {
 		return fmt.Errorf("setServerURLAdmin: error setting HKLM value -> %w", err)
 	}
-
 	return nil
 }
