34d839d81ae4b2316d617aee2c9a4479c054105f
Son Roy Almerol <github@snry.me>
2025-01-26 14:00:26 -0500

    remove custom mode and isDir

--- internal/agent/nfs/vssfs/file.go
+++ internal/agent/nfs/vssfs/file.go
@@ -15,16 +15,12 @@ type VSSFileInfo struct {
 	stableID uint64
 	name     string
 	size     int64
-	mode     os.FileMode
 	modTime  time.Time
-	isDir    bool
 }
 
 func (fi *VSSFileInfo) Name() string       { return fi.name }
 func (fi *VSSFileInfo) Size() int64        { return fi.size }
-func (fi *VSSFileInfo) Mode() os.FileMode  { return fi.mode }
 func (fi *VSSFileInfo) ModTime() time.Time { return fi.modTime }
-func (fi *VSSFileInfo) IsDir() bool        { return fi.isDir }
 func (vi *VSSFileInfo) Sys() interface{} {
 	nlink := uint32(1)
 	if vi.IsDir() {
@@ -42,30 +38,12 @@ func (vi *VSSFileInfo) Sys() interface{} {
 }
 
 func createFileInfoFromFindData(name string, fd *windows.Win32finddata) os.FileInfo {
-	var mode os.FileMode
-
-	// Set base permissions
-	if fd.FileAttributes&windows.FILE_ATTRIBUTE_READONLY != 0 {
-		mode = 0444 // Read-only for everyone
-	} else {
-		mode = 0666 // Read-write for everyone
-	}
-
-	// Add directory flag and execute permissions
-	if fd.FileAttributes&windows.FILE_ATTRIBUTE_DIRECTORY != 0 {
-		mode |= os.ModeDir | 0111 // Add execute bits for traversal
-		// Set directory-specific permissions
-		mode = (mode & 0666) | 0111 | os.ModeDir // Final mode: drwxr-xr-x
-	}
-
 	size := int64(fd.FileSizeHigh)<<32 + int64(fd.FileSizeLow)
 	modTime := time.Unix(0, fd.LastWriteTime.Nanoseconds())
 
 	return &VSSFileInfo{
 		name:    name,
 		size:    size,
-		mode:    mode,
 		modTime: modTime,
-		isDir:   mode&os.ModeDir != 0,
 	}
 }
