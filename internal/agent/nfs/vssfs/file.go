b26b6985dbacc23a76f77f13f6fb53240751ed67
Son Roy Almerol <sralmerol@sgl.com>
2025-01-28 14:16:34 -0500

    ensure vssfs open has backup privileges and sequential read

--- internal/agent/nfs/vssfs/file.go
+++ internal/agent/nfs/vssfs/file.go
@@ -5,11 +5,56 @@ package vssfs
 import (
 	"io/fs"
 	"os"
+	"runtime"
+	"sync"
 	"time"
+	"unsafe"
 
 	"github.com/willscott/go-nfs/file"
 	"golang.org/x/sys/windows"
 )
+
+var (
+	kernel32DLL    = windows.NewLazySystemDLL("kernel32.dll")
+	lockFileExProc = kernel32DLL.NewProc("LockFileEx")
+	unlockFileProc = kernel32DLL.NewProc("UnlockFile")
+)
+
+type vssfile struct {
+	*os.File
+	m sync.Mutex
+}
+
+const (
+	lockfileExclusiveLock = 0x2
+)
+
+func (f *vssfile) Lock() error {
+	f.m.Lock()
+	defer f.m.Unlock()
+
+	var overlapped windows.Overlapped
+	// err is always non-nil as per sys/windows semantics.
+	ret, _, err := lockFileExProc.Call(f.File.Fd(), lockfileExclusiveLock, 0, 0xFFFFFFFF, 0,
+		uintptr(unsafe.Pointer(&overlapped)))
+	runtime.KeepAlive(&overlapped)
+	if ret == 0 {
+		return err
+	}
+	return nil
+}
+
+func (f *vssfile) Unlock() error {
+	f.m.Lock()
+	defer f.m.Unlock()
+
+	// err is always non-nil as per sys/windows semantics.
+	ret, _, err := unlockFileProc.Call(f.File.Fd(), 0, 0, 0xFFFFFFFF, 0)
+	if ret == 0 {
+		return err
+	}
+	return nil
+}
 
 type VSSFileInfo struct {
 	stableID uint64
