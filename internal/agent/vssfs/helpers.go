252f59b1d2f8b290253baf24c2c4a09edc3b78f0
Son Roy Almerol <github@snry.me>
2025-02-26 22:00:41 -0500

    use go-json for encoding

--- internal/agent/vssfs/helpers.go
+++ internal/agent/vssfs/helpers.go
@@ -3,17 +3,15 @@
 package vssfs
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
-	"reflect"
 	"time"
 
+	"github.com/goccy/go-json"
 	"github.com/sonroyaalmerol/pbs-plus/internal/arpc"
 	"github.com/sonroyaalmerol/pbs-plus/internal/syslog"
-	"github.com/sonroyaalmerol/pbs-plus/internal/utils"
 	"github.com/valyala/fastjson"
 	"golang.org/x/sys/windows"
 )
@@ -106,111 +104,34 @@ func createFileInfoFromHandleInfo(path string, fd *windows.ByHandleFileInformati
 	}
 }
 
-// encodeJsonValue builds a JSON value using only the fastjson API.
 func encodeJsonValue(v interface{}) *fastjson.Value {
-	arena := new(fastjson.Arena)
-	var value *fastjson.Value
-
-	switch val := v.(type) {
-	case string:
-		value = arena.NewString(val)
-	case int:
-		value = arena.NewNumberInt(val)
-	case int64:
-		value = arena.NewNumberInt(int(val))
-	case bool:
-		if val {
-			value = arena.NewTrue()
-		} else {
-			value = arena.NewFalse()
-		}
-	case uint64:
-		value = arena.NewNumberFloat64(float64(val))
-	case map[string]interface{}:
-		obj := arena.NewObject()
-		for key, v2 := range val {
-			obj.Set(key, encodeJsonValue(v2))
-		}
-		value = obj
-	case map[string]string:
-		obj := arena.NewObject()
-		for key, v2 := range val {
-			obj.Set(key, arena.NewString(v2))
-		}
-		value = obj
-	case map[string]uint64:
-		obj := arena.NewObject()
-		for key, v2 := range val {
-			obj.Set(key, arena.NewNumberFloat64(float64(v2)))
-		}
-		value = obj
-	case *utils.FSStat:
-		obj := arena.NewObject()
-		obj.Set("total_size", arena.NewNumberInt(int(val.TotalSize)))
-		obj.Set("free_size", arena.NewNumberInt(int(val.FreeSize)))
-		obj.Set("available_size", arena.NewNumberInt(int(val.AvailableSize)))
-		obj.Set("total_files", arena.NewNumberInt(val.TotalFiles))
-		obj.Set("free_files", arena.NewNumberInt(val.FreeFiles))
-		obj.Set("available_files", arena.NewNumberInt(val.AvailableFiles))
-		// Represent CacheHint as a number (nanoseconds)
-		obj.Set("cache_hint", arena.NewNumberInt(int(val.CacheHint)))
-		value = obj
-	case *arpc.SerializableError:
-		obj := arena.NewObject()
-		obj.Set("error_type", arena.NewString(val.ErrorType))
-		obj.Set("message", arena.NewString(val.Message))
-		if val.Op != "" {
-			obj.Set("op", arena.NewString(val.Op))
-		}
-		if val.Path != "" {
-			obj.Set("path", arena.NewString(val.Path))
-		}
-		value = obj
-	case VSSFileInfo:
-		obj := arena.NewObject()
-		obj.Set("name", arena.NewString(val.Name))
-		obj.Set("size", arena.NewNumberInt(int(val.Size)))
-		// Convert fs.FileMode to int
-		obj.Set("mode", arena.NewNumberInt(int(val.Mode)))
-		obj.Set("modTime", arena.NewNumberInt(int(val.ModTime)))
-		if val.IsDir {
-			obj.Set("isDir", arena.NewTrue())
-		} else {
-			obj.Set("isDir", arena.NewFalse())
-		}
-		value = obj
-	case *VSSFileInfo:
-		obj := arena.NewObject()
-		obj.Set("name", arena.NewString(val.Name))
-		obj.Set("size", arena.NewNumberInt(int(val.Size)))
-		obj.Set("mode", arena.NewNumberInt(int(val.Mode)))
-		obj.Set("modTime", arena.NewNumberInt(int(val.ModTime)))
-		if val.IsDir {
-			obj.Set("isDir", arena.NewTrue())
-		} else {
-			obj.Set("isDir", arena.NewFalse())
-		}
-		value = obj
-	case []byte:
-		// Encode the []byte as a base64 string so that clients unmarshaling
-		// into a []byte field correctly decode it.
-		encoded := base64.StdEncoding.EncodeToString(val)
-		value = arena.NewString(encoded)
-	default:
-		// Handle slice types (e.g. []*VSSFileInfo) via reflection.
-		rv := reflect.ValueOf(v)
-		if rv.Kind() == reflect.Slice {
-			arr := arena.NewArray()
-			for i := 0; i < rv.Len(); i++ {
-				arr.SetArrayItem(i, encodeJsonValue(rv.Index(i).Interface()))
-			}
-			value = arr
-		} else {
-			value = arena.NewString(fmt.Sprintf("%v", v))
-		}
+	// If the value is already a *fastjson.Value, return it.
+	if fval, ok := v.(*fastjson.Value); ok {
+		return fval
 	}
 
-	return value
+	// Handle nil values by returning a JSON null.
+	if v == nil {
+		var n fastjson.Value
+		return &n
+	}
+
+	// Marshal the value to JSON bytes.
+	b, err := json.Marshal(v)
+	if err != nil {
+		// If marshaling fails, return a JSON object with an error message.
+		errMsg := fmt.Sprintf(`{"error": "failed to marshal value: %v"}`, err)
+		b = []byte(errMsg)
+	}
+
+	var p fastjson.Parser
+	val, err := p.ParseBytes(b)
+	if err != nil {
+		// If parsing fails, return a JSON object indicating the failure.
+		errStr := fmt.Sprintf(`{"error": "failed to parse json: %v"}`, err)
+		val, _ = p.Parse(errStr)
+	}
+	return val
 }
 
 // --- Error Response Helpers ---
