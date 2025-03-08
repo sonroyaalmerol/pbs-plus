26c156a6353e9f3757fd38ce095d10e8b154b16e
Son Roy Almerol <github@snry.me>
2025-03-07 21:50:08 -0500

    update skip count

--- internal/syslog/syslog_windows.go
+++ internal/syslog/syslog_windows.go
@@ -13,7 +13,7 @@ import (
 func init() {
 	// Configure zerolog to output via our EventLogWriter wrapped in a ConsoleWriter.
 	zlogger := zerolog.New(zerolog.NewConsoleWriter()).With().
-		CallerWithSkipFrameCount(4).
+		CallerWithSkipFrameCount(3).
 		Timestamp().
 		Logger()
 
@@ -34,7 +34,7 @@ func (l *Logger) SetServiceLogger(s service.Service) error {
 		w.Out = &LogWriter{logger: logger}
 		w.NoColor = true
 	})).With().
-		CallerWithSkipFrameCount(4).
+		CallerWithSkipFrameCount(3).
 		Timestamp().
 		Logger()
 
