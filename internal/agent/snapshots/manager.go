df5b32a11cd81bbbace4f2e1876ba98c0b0a5691
Son Roy Almerol <github@snry.me>
2025-03-08 18:54:50 -0500

    remove cephfs support for now

--- internal/agent/snapshots/manager.go
+++ internal/agent/snapshots/manager.go
@@ -14,17 +14,16 @@ type SnapshotManager struct {
 func NewSnapshotManager() *SnapshotManager {
 	return &SnapshotManager{
 		handlerMap: map[string]SnapshotHandler{
-			"cephfs": &CephfsSnapshotHandler{},
-			"btrfs":  &BtrfsSnapshotHandler{},
-			"zfs":    &ZFSSnapshotHandler{},
-			"lvm":    &LVMSnapshotHandler{},
-			"ext4":   &EXT4XFSHandler{}, // EXT4 delegates to LVM
-			"xfs":    &EXT4XFSHandler{}, // XFS delegates to LVM
-			"ntfs":   &NtfsSnapshotHandler{},
-			"refs":   &NtfsSnapshotHandler{},
-			"fat32":  nil, // FAT32 does not support snapshots
-			"exfat":  nil, // exFAT does not support snapshots
-			"hfs+":   nil, // HFS+ does not support snapshots
+			"btrfs": &BtrfsSnapshotHandler{},
+			"zfs":   &ZFSSnapshotHandler{},
+			"lvm":   &LVMSnapshotHandler{},
+			"ext4":  &EXT4XFSHandler{}, // EXT4 delegates to LVM
+			"xfs":   &EXT4XFSHandler{}, // XFS delegates to LVM
+			"ntfs":  &NtfsSnapshotHandler{},
+			"refs":  &NtfsSnapshotHandler{},
+			"fat32": nil, // FAT32 does not support snapshots
+			"exfat": nil, // exFAT does not support snapshots
+			"hfs+":  nil, // HFS+ does not support snapshots
 		},
 	}
 }
