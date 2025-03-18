a5fff2d39832647b995420a74e34f2304aac147c
Son Roy Almerol <sralmerol@sgl.com>
2025-03-18 10:46:21 -0400

    remove plus error

--- internal/proxy/views/pre/2_utils.js
+++ internal/proxy/views/pre/2_utils.js
@@ -1,11 +1,6 @@
 Ext.define('PBS.PlusUtils', {
   singleton: true,
   render_task_status: function(value, metadata, record, rowIndex, colIndex, store) {
-    var lastPlusError = record.data['last-plus-error'] || store.getById('last-plus-error')?.data.value
-    if (lastPlusError && (record.data['last-run-endtime'] || store.getById('last-run-endtime')?.data.value)) {
-      return `<i class="fa fa-times critical"></i> ${lastPlusError}`;
-    }
-
 	  if (
 	    !record.data['last-run-upid'] &&
 	    !store.getById('last-run-upid')?.data.value &&
